9fd77250fa73de35a79d4e30d12965adb3b89f13
Aditya <aditya.s274adi@gmail.com>
2026-03-09 01:39:51 +0530

    fix: add trailingSlash to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import("next").NextConfig} */
 const config = {
   output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default config;
+const config = {
+  output: 'export',
   images: {
     unoptimized: true,
   },
