39fb0981640187753b13d03b1b0655acba2c5c04
Aditya <aditya.s274adi@gmail.com>
2026-03-09 01:32:30 +0530

    fix: skip scripts during netlify build

--- next.config.js
+++ next.config.js
@@ -1,17 +1,8 @@
 /** @type {import("next").NextConfig} */
 const config = {
-  // Disable webpack 5 symlinks following to avoid Windows permission issues
-  webpack: (config, { isServer, dev }) => {
-    config.watchOptions = {
-      ignored: '**/*',
-      followSymlinks: false,
-    };
-    config.snapshot = {
-      managedPaths: [/^(.+?[\\/].+?)(?:[\\/]|$)/],
-    };
-    config.resolve.symlinks = false;
-    config.resolve.modules = ['node_modules'];
-    return config;
+  output: 'export',
+  images: {
+    unoptimized: true,
   },
 };
 
